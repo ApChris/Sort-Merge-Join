fd9d13903bba2b64e222c7437447c41680549a55
ApChris <achris.info@gmail.com>
2019-10-22 01:38:51 +0300

    key & payload fixed

--- src/getQolumn.c
+++ src/getQolumn.c
@@ -16,15 +16,15 @@ void getQolumn(int64_t ** array, int64_t rows, int64_t selected_column, relation
     // For each row
     while(i < rows)
     {
-        // key is the rowID
-        rel -> tuples[i].key = i;
+        // key is the value
+        rel -> tuples[i].key = array[i][selected_column];
 
-        // payload is the value of current array position
-        rel -> tuples[i].payload = array[i][selected_column];
+        // payload is the rowID
+        rel -> tuples[i].payload = i;
         i++;
     }
 
-    // The number of tuples is the number of rows 
+    // The number of tuples is the number of rows
     rel -> num_tuples = rows;
 
 }
