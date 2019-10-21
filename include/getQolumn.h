48816569dd74b0b5ec62c9e88516190d1edbd0d1
ApChris <achris.info@gmail.com>
2019-10-21 20:17:16 +0300

    Initial Commit

---
+++ include/getQolumn.h
@@ -0,0 +1,10 @@
+#ifndef _getQolumn_H
+#define _getQolumn_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/relation.h"
+
+void getQolumn(int64_t ** array, int64_t rows, int64_t selected_column, relation *rel);
+
+#endif
