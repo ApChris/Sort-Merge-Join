53dd982d7289e312ca53b30a634ded14df0944a8
ApChris <achris.info@gmail.com>
2019-10-22 01:13:45 +0300

    Initial Commit

---
+++ include/initArray.h
@@ -0,0 +1,11 @@
+#ifndef _INITARRAY_H
+#define _INITARRAY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../include/initArray.h"
+
+int64_t ** initArray(int64_t rows, int64_t columns);
+
+#endif
