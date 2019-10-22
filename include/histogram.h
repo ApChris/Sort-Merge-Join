954cce307aab2d101252458b0c21cfea0d9020ab
ApChris <achris.info@gmail.com>
2019-10-23 01:51:08 +0300

    a new struct has been added in histogram.h ( fixed )

--- include/histogram.h
+++ include/histogram.h
@@ -2,13 +2,21 @@
 #define _HISTOGRAM_H
 
 #include <stdint.h>
-
-typedef struct histogram
+#include "relation.h"
+typedef struct hist_tuple
 {
     uint64_t key;
     uint64_t payload;
     uint64_t sum;
-}relation;
+}hist_tuple;
 
 
+typedef struct histogram
+{
+    hist_tuple * hist_tuples;
+    uint64_t num_tuples;
+}histogram;
+
+void Histogram(relation * rel, histogram * hist);
+void Print_Histogram(histogram * hist);
 #endif
