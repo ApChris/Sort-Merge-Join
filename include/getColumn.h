3d18b13a7b36eee851f9c52820d95418fb78ee4a
ApChris <achris.info@gmail.com>
2019-10-23 01:51:53 +0300

    fixed

--- include/getColumn.h
+++ include/getColumn.h
@@ -1,8 +1,6 @@
 #ifndef _GETCOLUMN_H
 #define _GETCOLUMN_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "../include/relation.h"
 
 void getColumn(uint64_t ** array, uint64_t rows, uint64_t selected_column, relation *rel);
