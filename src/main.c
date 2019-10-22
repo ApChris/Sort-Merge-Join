895755c27f589264cb8db15a1bb130995a5f2db1
ApChris <achris.info@gmail.com>
2019-10-22 14:04:17 +0300

    change vars to uint64_t

--- src/main.c
+++ src/main.c
@@ -8,16 +8,16 @@ int main(int argc, char const *argv[])
     relation struct_R;
     relation *relation_R = &struct_R;
 
-    int64_t **array_R;
-    int64_t rows = 5;
-    int64_t columns = 10;
-    int64_t selected_column = 3;
+    uint64_t **array_R;
+    uint64_t rows = 5;
+    uint64_t columns = 10;
+    uint64_t selected_column = 3;
 
     array_R = initArray(rows,columns);
 
     for(int i=0; i < rows; i++)
     {
-        for (int64_t j = 0; j < columns; j++) {
+        for (uint64_t j = 0; j < columns; j++) {
             printf("%ld ", array_R[i][j]);
         }
         printf("\n");
@@ -25,7 +25,7 @@ int main(int argc, char const *argv[])
 
     getColumn(array_R,rows,selected_column,relation_R);
 
-    for (int64_t i = 0; i < rows; i++) {
+    for (uint64_t i = 0; i < rows; i++) {
         printf("%ld, %ld\n", relation_R->tuples[i].key, relation_R->tuples[i].payload);
     }
 
