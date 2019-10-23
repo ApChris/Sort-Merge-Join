00ef6e06d69b9458110f724c310306b7869bc740
ApChris <achris.info@gmail.com>
2019-10-23 19:25:20 +0300

    reordered has been added

--- src/main.c
+++ src/main.c
@@ -4,17 +4,19 @@
 #include "../include/initArray.h"
 #include "../include/histogram.h"
 #include "../include/psum.h"
+#include "../include/reorderedColumn.h"
+
 int main(int argc, char const *argv[])
 {
     relation struct_R;
     relation *relation_R = &struct_R;
 
     uint64_t **array_R;
-    uint64_t rows = 5;
+    uint64_t rows = 2500;
     uint64_t columns = 10;
     uint64_t selected_column = 3;
 
-    array_R = initArray(rows,columns);
+    array_R = InitArray(rows,columns);
 
     for(int i=0; i < rows; i++)
     {
@@ -24,7 +26,7 @@ int main(int argc, char const *argv[])
         printf("\n");
     }
 
-    getColumn(array_R,rows,selected_column,relation_R);
+    GetColumn(array_R,rows,selected_column,relation_R);
 
     for (uint64_t i = 0; i < rows; i++) {
         printf("%ld, %ld\n", relation_R->tuples[i].key, relation_R->tuples[i].payload);
@@ -36,9 +38,19 @@ int main(int argc, char const *argv[])
     histogram *hist = &struct_h;
     Histogram(relation_R,hist);
     Print_Histogram(hist);
+
     psum struct_p;
     psum *ps = &struct_p;
     Psum(hist,ps);
-    Print_Psum(ps);
+    Print_Psum(hist,ps);
+
+
+    relation struct_Rnew;
+    relation *relation_Rnew = &struct_Rnew;
+    ReorderedColumn(relation_R,relation_Rnew,ps);
+
+    for (uint64_t i = 0; i < rows; i++) {
+        printf("%ld, %ld\n", relation_Rnew->tuples[i].key, relation_Rnew->tuples[i].payload);
+    }
     return 0;
 }
