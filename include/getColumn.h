0c6a5920e0c62fbd8db010bfe8aab4022327657d
ApChris <achris.info@gmail.com>
2019-10-23 19:12:30 +0300

    declaration fixed

--- include/getColumn.h
+++ include/getColumn.h
@@ -3,6 +3,6 @@
 
 #include "../include/relation.h"
 
-void getColumn(uint64_t ** array, uint64_t rows, uint64_t selected_column, relation *rel);
+void GetColumn(uint64_t ** array, uint64_t rows, uint64_t selected_column, relation *rel);
 
 #endif
