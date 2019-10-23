fc714a089db953e065d51fb26cc68fab754cb078
ApChris <achris.info@gmail.com>
2019-10-23 19:26:30 +0300

    reorderedColumn.h has been added

---
+++ include/reorderedColumn.h
@@ -0,0 +1,8 @@
+#ifndef _REORDEREDCOLUMN_H
+#define _REORDEREDCOLUMN_H
+
+#include "relation.h"
+#include "psum.h"
+void ReorderedColumn(relation * rel_old, relation * rel_new, psum * ps);
+
+#endif
