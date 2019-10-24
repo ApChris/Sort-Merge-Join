5c1629b9c6415fba9e0a0bf926337a1243d19c66
ApChris <achris.info@gmail.com>
2019-10-24 21:35:27 +0300

    Initial Commit, splitBucket.c

---
+++ src/splitBucket.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "../include/splitBucket.h"
+
+void Split_Bucket(relation * rel_old, histogram * hist, psum *ps, relation *rel_new, uint64_t start, uint64_t end)
+{
+    Clean_Relation(rel_old);
+
+    for(uint64_t i = start; i < end; i++)
+    {
+        rel_old -> tuples[i].key = rel_new -> tuples[i].key;
+        rel_old -> tuples[i].payload = rel_new -> tuples[i].payload;
+    }
+    rel_old -> num_tuples = end;
+    Histogram(rel_old,hist,1);
+    Print_Histogram(hist);
+
+    Psum(hist,ps);
+    Print_Psum(hist,ps);
+
+    //rel_new -> num_tuples = rel_old -> num_tuples;
+    uint64_t i = 0;
+    uint64_t selected_byte = 1;
+    uint64_t result = 0;
+
+    while(i < rel_old -> num_tuples)
+    {
+        result = (rel_old -> tuples[i].key >> (8*selected_byte) & 0xFF);
+
+        //  ps -> psum_tuples[result].position -> it means the position that we are going to write
+        rel_new -> tuples[ps -> psum_tuples[result].position].key = rel_old -> tuples[i].key;
+
+        rel_new -> tuples[ps -> psum_tuples[result].position].payload = rel_old -> tuples[i].payload;
+
+        ps -> psum_tuples[result].position++;
+        i++;
+    }
+
+    RestorePsum(hist, ps);  //psum's position returns to its initial values
+
+
+    //ReorderedColumn(rel_old,rel_new,ps);
+    printf("start: %ld, end: %ld\n", start, end);
+
+    rel_old -> num_tuples = rel_new -> num_tuples;
+
+}
