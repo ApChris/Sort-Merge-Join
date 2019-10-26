7f5b5a88bb33074f339f8d2e9117405e6b304310
ApChris <achris.info@gmail.com>
2019-10-26 17:31:22 +0300

    I have to fix that to work perfectly

--- src/quicksort.c
+++ src/quicksort.c
@@ -2,41 +2,91 @@
 #include <stdlib.h>
 #include "../include/relation.h"
 
-void swap(tuple *a, tuple *b){
-    tuple temp = *a;
+void swap(tuple ** a, tuple ** b)
+{
+
+    tuple * temp  = *a;
+
     *a = *b;
     *b = temp;
+
 }
 
-int partition(relation *R, int start, int end){
-	uint64_t pivot = R->tuples[end].key;
-	int i = start-1;
+uint64_t partition(relation * rel, int64_t start, int64_t end)
+{
+	int64_t pivot = rel -> tuples[end].key;
+	int64_t i = start - 1;
 
-	for (int j = start; j <= end - 1; j++)
+
+	for (uint64_t j = start; j <= end - 1; j++)
 	{
-		if (R->tuples[j].key < pivot)
+		if (rel -> tuples[j].key < pivot)
 		{
 			i++;
-			swap(&R->tuples[i], &R->tuples[j]);
+			swap(&rel -> tuples[i], &rel -> tuples[j]);
+
+
+
 		}
 	}
-	swap(&R->tuples[i+1], &R->tuples[end]);
+	swap(&rel -> tuples[i+1], &rel -> tuples[end]);
+
 	return (i+1);
 }
 
-void Quicksort(relation *R, int start, int end){
-
-	if (start < end)
-	{
-		int pi = partition(R, start, end);
-		Quicksort(R, start, pi-1);
-		Quicksort(R, pi+1, end);
-	}
+uint64_t choose_pivot(int64_t start, int64_t end)
+{
+    return ((start+end) / 2);
 }
 
-void printBucket(relation *R, int start, int end){
-	for (int i = start; i < end; i++)
+void Quicksort(relation * rel, int start, int end)
+{
+	if (start < end)
 	{
-		printf("%ld, %ld\n", R->tuples[i].key, R->tuples[i].payload);
+		int64_t partition_index = partition(rel, start, end);
+		Quicksort(rel, start, partition_index - 1);
+		Quicksort(rel, partition_index + 1, end);
+	}
+    // uint64_t key, i, j, pivot;
+    // if(start < end)
+    // {
+    //     pivot = choose_pivot(start,end);
+    //     swap(&rel->tuples[start].key,&rel->tuples[end].key);
+    //     //swap(&rel->tuples[start].payload,&rel->tuples[end].payload);
+    //     key = rel->tuples[start].key;
+    //     i = start + 1;
+    //     j = end;
+    //     while(i <= j)
+    //     {
+    //         while((i <= end) && (rel -> tuples[i].key <= key))
+    //         {
+    //             i++;
+    //         }
+    //         while((j >= start) && (rel -> tuples[j].key > key))
+    //         {
+    //             j--;
+    //         }
+    //         if(i < j)
+    //         {
+    //             swap(&rel->tuples[i].key,&rel->tuples[j].key);
+    //             swap(&rel->tuples[i].payload,&rel->tuples[j].payload);
+    //         }
+    //     }
+    //
+    //     swap(&rel->tuples[start].key,&rel->tuples[j].key);
+    //     swap(&rel->tuples[start].payload,&rel->tuples[j].payload);
+    //     Quicksort(rel, start, j - 1);
+    //
+    //  	Quicksort(rel, j + 1, end);
+    //
+    // }
+
+}
+
+void printBucket(relation * rel, uint64_t start, uint64_t end)
+{
+	for(uint64_t i = start; i < end; i++)
+	{
+		printf("%ld, %ld\n", rel -> tuples[i].key, rel -> tuples[i].payload);
 	}
 }
