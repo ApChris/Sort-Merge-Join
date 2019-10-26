dd06f2ed21fec99a8789996b609fefd4324bdc6d
ApChris <achris.info@gmail.com>
2019-10-26 17:34:53 +0300

    args have been fixed

--- include/quicksort.h
+++ include/quicksort.h
@@ -4,9 +4,10 @@
 #include <stdint.h>
 #include "../include/psum.h"
 
-void swap(tuple *a, tuple *b);
-int partition(relation *R, int start, int end);
-void Quicksort(relation *R, int start, int end);
-void printBucket(relation *R, int start, int end);
+void swap(tuple **a, tuple **b);
+uint64_t choose_pivot(uint64_t start, uint64_t end);
+uint64_t partition(relation * rel, int64_t start, int64_t end);
+void Quicksort(relation * rel, int64_t start, int64_t end);
+void printBucket(relation * rel, int64_t start, int64_t end);
 
 #endif
