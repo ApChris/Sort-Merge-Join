3e76d9e809c954861d0f38717748f6b0a9edf223
ApChris <achris.info@gmail.com>
2019-10-26 19:18:23 +0300

    qiocksort.h has been fixed

--- include/quicksort.h
+++ include/quicksort.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include "../include/psum.h"
 
-void swap(tuple **a, tuple **b);
-uint64_t choose_pivot(uint64_t start, uint64_t end);
+void swap(tuple *a, tuple *b);
+
 uint64_t partition(relation * rel, int64_t start, int64_t end);
 void Quicksort(relation * rel, int64_t start, int64_t end);
 void printBucket(relation * rel, int64_t start, int64_t end);
