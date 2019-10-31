82ba962632e9424a201a5def18d767b344eeda3b
ApChris <achris.info@gmail.com>
2019-10-31 21:30:19 +0200

    NEW function -> get input from dataset files

--- src/getColumn.c
+++ src/getColumn.c
@@ -29,3 +29,66 @@ void GetColumn(uint64_t ** array, uint64_t rows, uint64_t selected_column, relat
     rel -> num_tuples = rows;
 
 }
+
+
+void GetColumn_FromFILE(const char * filename, relation *rel)
+{
+    FILE *file;
+    int64_t rows = 0;
+    char ch;
+
+    if((file = fopen(filename,"r")) == NULL)
+    {
+        perror("GetColumn_FromFILE.c error:");
+        exit(-1);
+    }
+    printf("%s\n",filename);
+    ch = getc(file);
+    for(ch = getc(file); ch != EOF; ch = getc(file))
+    {
+        if(ch == '\n')
+        {
+            rows++;
+        }
+    }
+    rewind(file);
+    printf("rows = %ld\n",rows);
+
+    // We'll create space for rows number of tuples
+    if((rel -> tuples = (tuple *)malloc(rows * sizeof(tuple))) == NULL)
+    {
+        perror("GetColumn_FromFILE.c , first malloc\n");
+        exit(-1);
+    }
+
+    uint64_t key = 0;
+    uint64_t payload = 0;
+    int64_t i = 0;
+
+    fscanf(file, "%lu,%lu",&key,&payload);
+
+    while (!feof(file))
+    {
+
+        //printf("%lu -> %lu,%lu\n",i,key,payload);
+        fscanf(file, "%lu,%lu",&key,&payload);
+        rel -> tuples[i].key = key; // key is the value
+        rel -> tuples[i].payload = payload; // payload
+        i++;
+    }
+
+
+
+    // The number of tuples is the number of rows
+    rel -> num_tuples = rows;
+
+
+
+
+    if(fclose(file) != NULL)
+    {
+        perror("GetColumn_FromFILE.c error:");
+        exit(-1);
+    }
+
+}
