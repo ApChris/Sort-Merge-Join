d0491b8a65ba6ae3f6d9c60c10e7780d9f24aeab
ApChris <achris.info@gmail.com>
2019-10-31 21:28:44 +0200

    print bucket function has been added

--- include/splitBucket.h
+++ include/splitBucket.h
@@ -5,5 +5,5 @@
 #include "../include/psum.h"
 
 void Split_Bucket(relation * rel_old, histogram * hist, psum *ps, relation *rel_new, int64_t start, int64_t end, uint64_t sel_byte);
-
+void printBucket(relation * rel, int64_t start, int64_t end);
 #endif
