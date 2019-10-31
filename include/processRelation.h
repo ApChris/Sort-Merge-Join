d5d08003c40e37710f115afd2dae634b6a9efc69
ApChris <achris.info@gmail.com>
2019-10-31 21:27:18 +0200

    new arg has been added

--- include/processRelation.h
+++ include/processRelation.h
@@ -3,7 +3,7 @@
 
 #include "splitBucket.h"
 
-void ProcessRelation(relation * rel_old, histogram * hist, psum * ps, relation * rel_new);
+void ProcessRelation(relation * rel_old, histogram * hist, psum * ps, relation * rel_new,int64_t sel_byte);
 void Print_Relation(relation * rel, histogram * hist, psum * ps);
 
 
