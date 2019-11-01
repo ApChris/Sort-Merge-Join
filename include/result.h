46128a57201f54e85b4cd17f20dfb52dbd6b9b48
ApChris <achris.info@gmail.com>
2019-11-02 01:14:48 +0200

    names have been changed

--- include/result.h
+++ include/result.h
@@ -2,24 +2,27 @@
 #define _RESULT_H
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "../include/relation.h"
-
+#include "../include/psum.h"
 
 #define ROWS ((1024*1024) / 40)	/*	40:	3 columns of int64_t
-								 		1 pointer to the next result, 
+								 		1 pointer to the next result,
 										1 int64_t num_tuples	*/
 
-typedef struct result{
+typedef struct result
+{
 	uint64_t buffer[ROWS][3];	//key, rowIdR, rowIdS
 	struct result *next_result;
 	uint64_t num_results;
-} result;
+}result;
 
 
 result * resultInit();
 void insertNewResult(result *res);
 void pushJoinedElements(result *res, uint64_t key, uint64_t rowIdS, uint64_t rowIdR);
 void printResult(result *res);
-void Join(relation *relation_R, relation *relation_S, result *res);
+void Join(relation *rel_A, histogram *histA, psum *psA, relation *rel_B, histogram *histB, psum *psB, result *res);
 
 #endif
