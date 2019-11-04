cb519f9a8b9d7d973aaa56fe6adfb5850020b3d2
ApChris <achris.info@gmail.com>
2019-11-04 18:49:24 +0200

    Final Commit

--- src/splitBucket.c
+++ src/splitBucket.c
@@ -3,6 +3,7 @@
 
 void Split_Bucket(relation * rel_old, histogram * hist, psum *ps, relation *rel_new, int64_t start, int64_t end, uint64_t sel_byte)
 {
+    // Clean the area of the relation
     Clean_Relation(rel_old,start,end);
 
     for(int64_t i = start; i < end; i++)
@@ -11,18 +12,16 @@ void Split_Bucket(relation * rel_old, histogram * hist, psum *ps, relation *rel_
         rel_old -> tuples[i].payload = rel_new -> tuples[i].payload;
     }
 
-
+    // Create a new histogram in range start:end
     Histogram(rel_old,hist,sel_byte,start,end);
-    //Print_Histogram(hist);
 
-
+    // Create a new psum
     Psum(hist,ps,start);
-    //Print_Psum(hist,ps);
-
 
     uint64_t i = start;
     uint64_t result = 0;
 
+    // Get old keys and write them correclty at new relation
     while(i < end)
     {
 
@@ -43,7 +42,7 @@ void Split_Bucket(relation * rel_old, histogram * hist, psum *ps, relation *rel_
 
 }
 
-
+// Print a specific bucket
 void printBucket(relation * rel, int64_t start, int64_t end)
 {
 	for(int64_t i = start; i < end; i++)
