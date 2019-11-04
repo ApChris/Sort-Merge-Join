9588aa29d5d85c25398328d32ae6c2e789dc308d
ApChris <achris.info@gmail.com>
2019-11-05 01:33:45 +0200

    Initial Commit, sortMergeJoin.h

---
+++ include/sortMergeJoin.h
@@ -0,0 +1,10 @@
+#ifndef _SORT_MERGE_JOIN_H_
+#define _SORT_MERGE_JOIN_H_
+
+#include "relation.h"
+#include "result.h"
+#include <time.h>
+
+result * SortMergeJoin(relation * relation_A_final, relation * relation_B_final);
+
+#endif
