1afed95d4bb629be7072d8b2d792c6121adbc0fb
ApChris <achris.info@gmail.com>
2019-11-04 19:57:43 +0200

    Final Commit

--- src/cleanRelation.c
+++ src/cleanRelation.c
@@ -1,6 +1,7 @@
 
 #include "../include/cleanRelation.h"
 
+// A fucntion that clears a range of keys in a relation!
 void Clean_Relation(relation * rel, int64_t start, int64_t end)
 {
     int64_t i = start;
