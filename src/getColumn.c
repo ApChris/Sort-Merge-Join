fea0b5d860768962e14ccc845c09ba9846c1c985
ApChris <achris.info@gmail.com>
2019-11-04 19:38:52 +0200

    Final Commit

--- src/getColumn.c
+++ src/getColumn.c
@@ -42,7 +42,7 @@ void GetColumn_FromFILE(const char * filename, relation *rel)
         perror("GetColumn_FromFILE.c error:");
         exit(-1);
     }
-    //printf("%s\n",filename);
+
     ch = getc(file);
     for(ch = getc(file); ch != EOF; ch = getc(file))
     {
@@ -52,7 +52,6 @@ void GetColumn_FromFILE(const char * filename, relation *rel)
         }
     }
     rewind(file);
-    //printf("rows = %ld\n",rows);
 
     // We'll create space for rows number of tuples
     if((rel -> tuples = (tuple *)malloc(rows * sizeof(tuple))) == NULL)
@@ -65,33 +64,17 @@ void GetColumn_FromFILE(const char * filename, relation *rel)
     uint64_t payload = 0;
     int64_t i = 0;
 
-    // fscanf(file, "%lu,%lu",&key,&payload);
-    //
-    // while (!feof(file))
-    // {
-    //
-    //     printf("%lu -> %lu,%lu\n",i,key,payload);
-    //     fscanf(file, "%lu,%lu",&key,&payload);
-    //     rel -> tuples[i].key = key; // key is the value
-    //     rel -> tuples[i].payload = payload; // payload
-    //     i++;
-    // }
 
-    //ch = getc(file);
     for(; i < rows; i++)
     {
         fscanf(file, "%lu,%lu",&key,&payload);
-            rel -> tuples[i].key = key; // key is the value
-            rel -> tuples[i].payload = payload; // payload
-            //i++;
+        rel -> tuples[i].key = key; // key is the value
+        rel -> tuples[i].payload = payload; // payload
+
     }
-
-
 
     // The number of tuples is the number of rows
     rel -> num_tuples = rows;
-
-
 
 
     if(fclose(file) != NULL)
