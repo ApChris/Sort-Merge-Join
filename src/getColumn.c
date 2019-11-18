55538134b14452e5a8f7ed7f123b35c137faee3d
ApChris <achris.info@gmail.com>
2019-11-18 23:12:18 +0200

    Read init file

--- src/getColumn.c
+++ src/getColumn.c
@@ -84,3 +84,132 @@ void GetColumn_FromFILE(const char * filename, relation *rel)
     }
 
 }
+
+
+
+
+
+metadata * Read_Init_Binary(const char * filename)
+{
+
+    // variables for init file
+    FILE * file_init;
+    uint64_t rows = 0;
+    char current_path[4];
+    char ch;
+
+
+    // variables for binary files
+    FILE * file_binary;
+    uint64_t length_binary = 0;
+    uint64_t * array;
+
+
+    // variables for metadata struct
+    metadata * md;
+    uint64_t offset_metadata = 2;
+
+
+    if((file_init = fopen(filename,"rb+")) == NULL)
+    {
+        perror("file_init fopen failed:");
+        exit(-1);
+    }
+
+
+    // Get the rows of init file
+    ch = getc(file_init);
+    for(ch = getc(file_init); ch != EOF; ch = getc(file_init))
+    {
+        if(ch == '\n')
+        {
+            rows++;
+        }
+    }
+    rewind(file_init);
+
+
+    if((md = (uint64_t *)malloc(rows*sizeof(metadata))) == NULL)
+    {
+        perror("Read_binary_file malloc failed:");
+        exit(-1);
+    }
+
+    // for every binary file
+    for (size_t i = 0; i < rows; i++)
+    {
+        fscanf(file_init,"%s\n",current_path);
+        //printf("%s\n",current_path);
+        char final_path[22] = "workloads/small/";
+        strcat(final_path,current_path);
+
+        if((file_binary = fopen(final_path,"rb+")) == NULL)
+        {
+            perror("fopen failed:");
+            exit(-1);
+        }
+
+        // length == all bytes in file but we divide with 8 , because we use uint64_t
+        fseek(file_binary, 0, SEEK_END);
+        length_binary = ftell(file_binary);
+        fseek(file_binary, 0, SEEK_SET);
+
+
+        // Allocate memory
+        if((array = (uint64_t *)malloc((length_binary/8 + 1)*sizeof(uint64_t))) == NULL)
+        {
+            perror("File:malloc failed:");
+            exit(-1);
+        }
+        for (size_t i = 0; i < length_binary/8; i++)
+        {
+            array[i] = 0;
+        }
+
+        // start reading
+        fread(array,length_binary,1,file_binary);
+
+        md[i].num_tuples = array[0];
+        md[i].num_columns = array[1];
+        if((md[i].array = (uint64_t *)malloc(array[1]*sizeof(uint64_t *))) == NULL)
+        {
+            perror("File:malloc2 failed:");
+            exit(-1);
+        }
+        for (size_t j = 0; j < array[1]; j++)
+        {
+        //    printf("value = %lu --> address = %p\n",array[offset_metadata],&array[offset_metadata]);
+            md[i].array[j] = &array[offset_metadata];
+            offset_metadata += array[0];
+
+        }
+        //return md;
+        offset_metadata = 2;
+        // print
+        // for (size_t i = 2; i < array[0] + 2; i++)
+        // {
+        //     for (size_t j = 0; j < array[1]; j++)
+        //     {
+        //         printf("%lu|",array[i+offset]);
+        //         offset += array[0];
+        //     }
+        //     offset = 0;
+        //     printf("\n");
+        // }
+
+
+        // close
+        if(fclose(file_binary))
+        {
+            perror("fclose failed:");
+            exit(-1);
+        }
+
+
+
+    }
+
+    return md;
+
+
+}
