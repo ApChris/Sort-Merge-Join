80583c517eae9039006aad32f17e058ca49ded8e
ApChris <achris.info@gmail.com>
2019-11-18 22:32:53 +0200

    Initial Commit

---
+++ include/metadata.h
@@ -0,0 +1,14 @@
+#ifndef _METADATA_H_
+#define _METADATA_H_
+
+typedef struct metadata
+{
+    uint64_t num_tuples;
+    uint64_t num_columns;
+    uint64_t * array;
+}metadata;
+
+
+metadata * Read_Init_Binary(const char * filename);
+
+#endif
