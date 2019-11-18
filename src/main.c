a9d93786cdd148f862a18beb2e6202942d5cd087
ApChris <achris.info@gmail.com>
2019-11-18 23:11:56 +0200

    first main for project 2

--- src/main.c
+++ src/main.c
@@ -10,63 +10,27 @@
 #include "../include/sortMergeJoin.h"
 #include "../include/processRelation.h"
 
+
 int main(int argc, char const *argv[])
 {
-    relation struct_A;
-    relation * relation_A = &struct_A;
 
-    relation struct_A_final;
-    relation * relation_A_final = &struct_A_final;
+    metadata * md;
 
-    relation struct_B;
-    relation * relation_B = &struct_B;
+    uint64_t * ptr;
 
-    relation struct_B_final;
-    relation * relation_B_final = &struct_B_final;
+    md = Read_Init_Binary("workloads/small/small.init");
 
-    result struct_r;
-    result * res = &struct_r;
-
-    if(argc == 2)
+    for (size_t i = 0; i < 14; i++)
     {
-        if(!strcmp(argv[1],"tiny"))
+        printf("%lu) tuples = %lu\tcolumns = %lu\t----> ",i,  md[i].num_tuples, md[i].num_columns);
+
+        for (size_t j = 0; j < md[i].num_columns; j++)
         {
-            GetColumn_FromFILE("Datasets/tiny/relA",relation_A);
-            GetColumn_FromFILE("Datasets/tiny/relB",relation_B);
+            ptr = md[i].array[j];
+            printf("%lu|",ptr[j]);
         }
-        else if(!strcmp(argv[1],"small"))
-        {
-            GetColumn_FromFILE("Datasets/small/relA",relation_A);
-            GetColumn_FromFILE("Datasets/small/relB",relation_B);
-        }
-        else if(!strcmp(argv[1],"medium"))
-        {
-            GetColumn_FromFILE("Datasets/medium/relA",relation_A);
-            GetColumn_FromFILE("Datasets/medium/relB",relation_B);
-        }
-        else
-        {
-            printf("Wrong argument! Try again\n");
-            exit(-1);
-        }
-    }
-    else if(argc > 2)
-    {
-        printf("\nToo many arguments! Try again\n\n");
-        exit(-1);
-    }
-    else
-    {
-        printf("\nYou have to run one of the following:\n./smj tiny\n./smj small\n./smj medium\n\nTry again!!\n\n");
-        exit(-1);
+        printf("\n");
     }
 
-    Final_Relation(relation_A, relation_A_final);
-    Final_Relation(relation_B, relation_B_final);
-
-    res = SortMergeJoin(relation_A_final, relation_B_final);
-
-    //printResult(res);
-    Deallocate_List(res);
     return 0;
 }
