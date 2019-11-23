f9c36d97cd75bf27339944786bc88a2337c0899f
ApChris <achris.info@gmail.com>
2019-11-23 15:38:08 +0200

    declarations have been added

--- include/processRelation.h
+++ include/processRelation.h
@@ -5,10 +5,13 @@
 #include "splitBucket.h"
 #include "quicksort.h"
 #include "reorderedColumn.h"
+#include "metadata.h"
 
 void ProcessRelation(relation * rel_old, histogram * hist, psum * ps, relation * rel_new,int64_t sel_byte);
 void Print_Relation(relation * rel, histogram * hist, psum * ps);
 void Final_Relation(relation * rel, relation * rel_final);
 
+void Create_Relation(metadata * md, uint64_t md_pos,uint64_t array_pos, relation * rel);
+void Filter(relation * rel, uint64_t limit, char symbol, relation * rel_final);
 
 #endif
