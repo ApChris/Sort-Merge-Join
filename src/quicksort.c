46823a67c7e87af782af0e61054bbe38f5fe1c18
ApChris <achris.info@gmail.com>
2019-11-25 15:57:53 +0200

    quicksort_v2 has been added

--- src/quicksort.c
+++ src/quicksort.c
@@ -46,3 +46,50 @@ void Quicksort(relation * rel, int64_t start, int64_t end)
     }
 
 }
+
+
+
+void swap_v2(uint64_t * a, uint64_t * b)
+{
+    uint64_t  temp  = *a;
+    *a = *b;
+    *b = temp;
+
+}
+
+int64_t partition_v2(uint64_t * array, int64_t start, int64_t end)
+{
+	uint64_t pivot = array[end];
+
+	int64_t i = start - 1;
+
+	for (int64_t j = start ; j <= end - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i+1], &array[end]);
+
+	return (i+1);
+}
+
+// Sort keys from relation in range start to end
+void Quicksort_v2(uint64_t * array, int64_t start, int64_t end)
+{
+
+    if(array == NULL)
+    {
+        perror("Quicksort error:");
+        exit(-1);
+    }
+    if (start < end)
+    {
+        int64_t partition_index = partition(array, start, end);
+        Quicksort(array, start, partition_index - 1);
+        Quicksort(array, partition_index + 1, end);
+    }
+
+}
