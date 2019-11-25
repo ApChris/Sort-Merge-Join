98fef754458870fb69dd9bf433600f85bdd18e64
ApChris <achris.info@gmail.com>
2019-11-25 15:10:04 +0200

    updated main

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "../include/result.h"
 #include "../include/sortMergeJoin.h"
 #include "../include/processRelation.h"
+#include "../include/intervening.h"
 
 
 int main(int argc, char const *argv[])
@@ -35,39 +36,127 @@ int main(int argc, char const *argv[])
     printf("\n\n\n");
 
     Read_Work("workloads/small/small.work");
+//
+//     relation struct_A;
+//     relation * relation_A = &struct_A;
+//
+//     relation struct_A_final;
+//     relation * relation_A_final = &struct_A_final;
+//     Create_Relation(md,5,1,relation_A);
+//     Final_Relation(relation_A, relation_A_final);
+//
+//     for (size_t i = 0; i < relation_A_final -> num_tuples; i++)
+//     {
+//         printf("%lu)\t%lu\n", relation_A_final->tuples[i].payload, relation_A_final->tuples[i].key );
+//     }
+//
+//     relation struct_B;
+//     relation * relation_B = &struct_B;
+//
+//     relation struct_B_final;
+//     relation * relation_B_final = &struct_B_final;
+//     Create_Relation(md,0,0,relation_B);
+//     Final_Relation(relation_B, relation_B_final);
+//
+//
+//     relation struct_B_filtered;
+//     relation * relation_B_filtered = &struct_B_filtered;
+//
+//     Filter(relation_B_final,3000,'<',relation_B_filtered);
+//
+//
+//     for (size_t i = 0; i < relation_B_filtered -> num_tuples; i++)
+//     {
+//         printf("%lu)\t%lu\n", relation_B_filtered->tuples[i].payload, relation_B_filtered->tuples[i].key );
+//     }
+//
+//     intervening *interveningA = intervening_Init();
+//      intervening_array *array_A = intervening_Array_Init();
+//       intervening_array *array_B = intervening_Array_Init();
+//     uint64_t counter = Join_v2(interveningA,relation_A_final,relation_B_filtered,array_A,array_B);
+//     printf("counter = %lu\n",counter);
+//
+// for (size_t i = 0; i < array_A -> position; i++)
+// {
+//     /* code */
+//     printf("%lu\n",array_A -> payload_array[i]);
+// }
+//
+// uint64_t sum = 0;
+//
+// sum = Sum_Column(array_A);
+//
+// printf("Sum = %lu\n",sum);
+
+
+
+
+
+
+
+
+// 5 1|0.1=1.0&0.2=4531|1.2
+// result 9283
+
 
     relation struct_A;
     relation * relation_A = &struct_A;
 
     relation struct_A_final;
     relation * relation_A_final = &struct_A_final;
-    Create_Relation(md,3,3,relation_A);
+    Create_Relation(md,5,1,relation_A);
     Final_Relation(relation_A, relation_A_final);
 
-    for (size_t i = 0; i < relation_A_final -> num_tuples; i++)
-    {
-        printf("%lu)\t%lu\n", relation_A_final->tuples[i].payload, relation_A_final->tuples[i].key );
-    }
+    relation struct_A_2;
+    relation * relation_A_2 = &struct_A_2;
+
+    relation struct_A_final_2;
+    relation * relation_A_final_2 = &struct_A_final_2;
+    Create_Relation(md,5,2,relation_A_2);
+    Final_Relation(relation_A_2, relation_A_final_2);
+
+    // for (size_t i = 0; i < relation_A_final -> num_tuples; i++)
+    // {
+    //     printf("%lu)\t%lu\n", relation_A_final->tuples[i].payload, relation_A_final->tuples[i].key );
+    // }
 
     relation struct_B;
     relation * relation_B = &struct_B;
 
     relation struct_B_final;
     relation * relation_B_final = &struct_B_final;
-    Create_Relation(md,0,0,relation_B);
+    Create_Relation(md,1,0,relation_B);
     Final_Relation(relation_B, relation_B_final);
 
 
-    relation struct_B_filtered;
-    relation * relation_B_filtered = &struct_B_filtered;
+    relation struct_A_filtered;
+    relation * relation_A_filtered = &struct_A_filtered;
 
-    Filter(relation_B_final,3000,'>',relation_B_filtered);
+    Filter(relation_A_final_2,4531,'=',relation_A_filtered);
 
 
-    for (size_t i = 0; i < relation_B_filtered -> num_tuples; i++)
+    for (size_t i = 0; i < relation_A_filtered -> num_tuples; i++)
     {
-        printf("%lu)\t%lu\n", relation_B_filtered->tuples[i].payload, relation_B_filtered->tuples[i].key );
+        printf("%lu)\t%lu\n", relation_A_filtered->tuples[i].payload, relation_A_filtered->tuples[i].key );
     }
+
+    intervening *interveningA = intervening_Init();
+    intervening_array *array_A = intervening_Array_Init();
+    intervening_array *array_B = intervening_Array_Init();
+    uint64_t counter = Join_v2(interveningA,relation_A_final,relation_B_final,array_A,array_B);
+    printf("counter = %lu\n",counter);
+
+for (size_t i = 0; i < array_A -> position; i++)
+{
+    /* code */
+    printf("%lu\n",array_A -> payload_array[i]);
+}
+
+uint64_t sum = 0;
+
+sum = Sum_Column(array_A);
+
+printf("Sum = %lu\n",sum);
 
 
     return 0;
