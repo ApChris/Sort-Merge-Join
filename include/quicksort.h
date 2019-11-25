4ade068f0d09e2d68ae079fe20c103f041683fa3
ApChris <achris.info@gmail.com>
2019-11-25 15:58:47 +0200

    quicksort_v2 declaration has been added too

--- include/quicksort.h
+++ include/quicksort.h
@@ -8,4 +8,7 @@ void swap(tuple *a, tuple *b);
 int64_t partition(relation * rel, int64_t start, int64_t end);
 void Quicksort(relation * rel, int64_t start, int64_t end);
 
+void swap_v2(uint64_t * a, uint64_t * b);
+int64_t partition_v2(uint64_t * array, int64_t start, int64_t end);
+void Quicksort_v2(uint64_t * array, int64_t start, int64_t end);
 #endif
