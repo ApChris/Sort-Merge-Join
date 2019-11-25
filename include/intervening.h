c1b9cc1764374fe1564efdba9945fbaa30f1dc96
ApChris <achris.info@gmail.com>
2019-11-25 15:08:54 +0200

    intervening.h has been updated

--- include/intervening.h
+++ include/intervening.h
@@ -7,22 +7,25 @@
 #include "relation.h"
 
 
-typedef struct intervening_array{
+typedef struct intervening_array
+{
 	uint64_t position;
-	uint64_t *payload_array;
+	uint64_t * payload_array;
 }intervening_array;
 
-typedef struct intervening_struct{
+typedef struct intervening
+{
 	uint64_t total_arrays;
-	intervening_array **rowId;
-}intervening_struct;
+	intervening_array * rowId;
+}intervening;
 
-intervening_array *interveningArrayInit();
-intervening_struct *interveningStructInit();
+intervening_array * intervening_Array_Init();
+intervening * intervening_Init();
 
-intervening_struct *updateInterveningStruct(intervening_struct *intervening_struct, uint64_t *array_A, uint64_t *array_B);
-void pushJoinedElements_v2(uint64_t payload_A, uint64_t payload_B, intervening_array *array_A, intervening_array *array_B);
-void Join_v2(intervening_struct *intervening_struct, relation *rel_A, relation *rel_B, intervening_array *array_A, intervening_array *array_B);
+intervening * updateInterveningStruct(intervening *intervening, uint64_t *array_A, uint64_t *array_B);
+void pushJoinedElements_v2(uint64_t payload, intervening_array * array);
+uint64_t Join_v2(intervening *intervening, relation *rel_A, relation *rel_B, intervening_array *array_A, intervening_array *array_B);
 
-
+uint64_t binarySearch(uint64_t * array,uint64_t start, uint64_t end, uint64_t payload);
+uint64_t Sum_Column(intervening_array * array);
 #endif
