d593ddc0be61a05a9fe41ca20642b59aab509895
ApChris <achris.info@gmail.com>
2019-11-26 22:54:31 +0200

    payload variable has been changed with dynamic array of payloads.

--- src/reorderedColumn.c
+++ src/reorderedColumn.c
@@ -19,14 +19,25 @@ void ReorderedColumn(relation * rel_old, relation * rel_new, psum * ps)
 
     while(i < rel_old -> num_tuples)
     {
+
         result = (rel_old -> tuples[i].key >> (8*selected_byte) & 0xFF);
 
         //  ps -> psum_tuples[result].position -> it means the position that we are going to write
         rel_new -> tuples[ps -> psum_tuples[result].position].key = rel_old -> tuples[i].key;
 
-        //rel_new -> tuples[ps -> psum_tuples[result].position].payload = rel_old -> tuples[i].payload;
-        rel_new -> tuples[ps -> psum_tuples[result].position].payload = rel_old -> tuples[i].payload;
+        //if(rel_old -> tuples[i].position)
+        if((rel_new -> tuples[ps -> psum_tuples[result].position].payload = (uint64_t *)malloc((rel_old -> tuples[i].position) *sizeof(uint64_t))) == NULL)
+        {
+            perror("reordered.c , 2nd malloc\n");
+            exit(-1);
+        }
+        for (size_t j = 0; j < rel_old -> tuples[i].position; j++)
+        {
+            rel_new -> tuples[ps -> psum_tuples[result].position].payload[j] = rel_old -> tuples[i].payload[j];// payload
+        }
+        printf("\n");
 
+        rel_new -> tuples[ps -> psum_tuples[result].position].position = rel_old -> tuples[i].position;
         ps -> psum_tuples[result].position++;
         i++;
     }
