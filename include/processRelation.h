eac3c9dcd619c25bb5061c9558c50e795cdfc56c
ApChris <achris.info@gmail.com>
2019-11-26 23:15:55 +0200

    Update_tuple_payload and Print_Relation_2 statements added.  Final Relation changed to Radix Sort

--- include/processRelation.h
+++ include/processRelation.h
@@ -9,9 +9,14 @@
 
 void ProcessRelation(relation * rel_old, histogram * hist, psum * ps, relation * rel_new,int64_t sel_byte);
 void Print_Relation(relation * rel, histogram * hist, psum * ps);
-void Final_Relation(relation * rel, relation * rel_final);
+void Radix_Sort(relation * rel, relation * rel_final);
+
+
+
 
 void Create_Relation(metadata * md, uint64_t md_pos,uint64_t array_pos, relation * rel);
+void Print_Relation_2(relation * rel);
+void Update_Tuple_Payload(metadata * md, relation * rel, uint64_t pos, uint64_t key, uint64_t payload);
 void Filter(relation * rel, uint64_t limit, char symbol, relation * rel_final);
 
 #endif
