77df7d954e7244ae4702fc688ff1dc6b433735b4
ApChris <achris.info@gmail.com>
2019-12-01 18:05:22 +0200

    statements have been updated

--- include/processRelation.h
+++ include/processRelation.h
@@ -18,5 +18,6 @@ void Create_Relation(metadata * md, uint64_t md_pos,uint64_t array_pos, relation
 void Print_Relation_2(relation * rel);
 void Update_Tuple_Payload(metadata * md, relation * rel, uint64_t pos, uint64_t key, uint64_t payload);
 relation * Filter(relation * rel, uint64_t limit, char symbol);
-
+void Update_Relation_Keys(metadata * md, uint64_t md_row, uint64_t md_column, relation * rel, uint64_t pos);
+void CheckSum(metadata * md, uint64_t md_row, uint64_t md_column, relation * rel, uint64_t pos);
 #endif
