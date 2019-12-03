b305a6ff6cef36c00c5ac293ff130d9a7db0bf31
ApChris <achris.info@gmail.com>
2019-12-03 16:17:45 +0200

    statements have been updated

--- include/getColumn.h
+++ include/getColumn.h
@@ -4,11 +4,15 @@
 #include <string.h>
 #include "relation.h"
 #include "metadata.h"
+#include "work.h"
 
 // functions to get input
 void GetColumn(uint64_t ** array, uint64_t rows, uint64_t selected_column, relation *rel);
 void GetColumn_FromFILE(const char * filename, relation *rel);
 
 metadata * Read_Init_Binary(const char * filename);
-void Read_Work(const char * filename);
+work_line * Read_Work(const char * filename);
+
+uint64_t self_join_check(char * str1, char * str2);
+char * split(char * str, const char * delim);
 #endif
