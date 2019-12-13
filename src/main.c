b4ec1bb36ba52e00fd239c62e91b4b8df0ec893e
ApChris <achris.info@gmail.com>
2019-12-13 14:36:46 +0200

    updated main.c for small.work

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 #include "../include/sortMergeJoin.h"
 #include "../include/processRelation.h"
 #include "../include/intervening.h"
+#include "../include/work.h"
+#include "../include/execute_query.h"
 
 
 int main(int argc, char const *argv[])
@@ -41,72 +43,148 @@ int main(int argc, char const *argv[])
 
     // 3 0 1|0.2=1.0&0.1=2.0&0.2>3499|1.2 0.1
 
+
     // r3.2
-    relation struct_A;
-    relation * relation_A = &struct_A;
-    Create_Relation(md,3,2,relation_A);
 
-    // Radix sort to r3.2
-    relation struct_A_final;
-    relation * relation_A_final = &struct_A_final;
-    relation_A = Radix_Sort(relation_A, relation_A_final);
+    relation * relation_A;// = &struct_A;// = &struct_A;
 
-    // 0.2>3499
-    relation_A = Filter(relation_A, 3499, '>');
 
-    Print_Relation_2(relation_A);
+    relation_A = Create_Relation(md,0,0);
+    //Print_Relation_2(relation_A);
+    printf("Edw\n");
+    relation_A = Radix_Sort(relation_A);
+    //Print_Relation_2(relation_A);
 
-    // // r0.0
-    relation struct_B;
-    relation * relation_B = &struct_B;
-    Create_Relation(md,0,0,relation_B);
-    //
-    // // Radix Sort to r0.0
-    relation struct_B_final;
-    relation * relation_B_final = &struct_B_final;
-    relation_B = Radix_Sort(relation_B, relation_B_final);
-    //
 
-    Print_Relation_2(relation_B);
-    // // 0.2 = 1.0
+    relation * relation_B;// = &struct_A;
+    relation_B = Create_Relation(md,1,0);
+
+
+    relation_B = Radix_Sort(relation_B);
+
+
+//    Print_Relation_2(relation_B);
+
+
     intervening * interv_final = interveningInit();
-    Join_v2(interv_final, relation_A, relation_B, 3, 0);
-    Print_Relation_2(interv_final->final_rel);
-    // // 0.1
-    Update_Relation_Keys(md,3,1,interv_final->final_rel,0);
-    //
-    relation struct_D_final;
-    relation * relation_D_final = &struct_D_final;
-    interv_final -> final_rel = Radix_Sort(interv_final->final_rel, relation_D_final);
-    //
-    Print_Relation_2(interv_final->final_rel);
-    //
-    // // r0.0
-    relation struct_C;
-    relation * relation_C = &struct_C;
-    Create_Relation(md,1,0,relation_C);
-    //
-    // // Radix Sort to r0.0
-    relation struct_C_final;
-    relation * relation_C_final = &struct_C_final;
-    relation_C = Radix_Sort(relation_C, relation_C_final);
-    //
-    Join_v2(interv_final, interv_final->final_rel, relation_C, 3, 1);
+    Join_v2(interv_final, relation_A, relation_B, 0, 1);
 
-    Print_Relation_2(interv_final->final_rel);
+    //Print_Relation_2(interv_final -> final_rel);
+
+    relation * relation_C;// = &struct_A;
+    relation_C = Create_Relation(md,2,1);
+
+    relation_C = Radix_Sort(relation_C);
+
+
+    Update_Relation_Keys(md,0,0,interv_final ->final_rel,0);
+
+
+    interv_final ->final_rel = Radix_Sort(interv_final ->final_rel);
+    Join_v2(interv_final, interv_final -> final_rel, relation_C, 0, 2);
+
+    uint64_t x = CheckSum(md,0,0,interv_final->final_rel,0);
+    printf("%lu\n",x );
     //
-    CheckSum(md,0,2,interv_final->final_rel,1);
-    CheckSum(md,3,1,interv_final->final_rel,0);
-    printf("\n");
-    for (size_t i = 0; i < interv_final->position; i++)
-    {
-            printf("r%lu|",interv_final->rowId[i]);
-    }
-    printf("\n");
+    //Print_Relation_2(interv_final -> final_rel);
+    //printf("%lu\n", interv_final -> final_rel -> num_tuples);
+    //  intervening * interv_final = interveningInit();
+    // Join_v2(interv_final, relation_A[0], relation_A[1], 3, 0);
+    // Print_Relation_2(interv_final->final_rel);
+    // for (size_t i = 0; i < 4; i++)
+    // {
+    //     /* code */
+    //     relation ** relation_A;
+    //     relation_A = (relation **)malloc(sizeof(relation *)*2);
+    //     relation_A[0] = Create_Relation(md,3,1);
+    //     relation_A[1] = Create_Relation(md,3,2);
+    //
+    //
+    //
+    //     relation_A[0] = Radix_Sort(relation_A[0]);
+    //
+    //     //Create_Relation(md,3,2,relation_A[1]);
+    //
+    //
+    //     relation_A[1] = Radix_Sort(relation_A[1]);
+    //
+    //
+    // //    Print_Relation_2(relation_A[0]);
+    // }
+
+    //  relation_A[0] = &struct_A[0];
+    // relation_A[1] = &struct_A[1];
+
+    // relation ** relation_B[2];
+    // relation_B[0] = &struct_B;
+    // Create_Relation(md,3,2,relation_A[0]);
+    //
+    // // Radix sort to r3.2
+    // // relation struct_A_final;
+    // // relation * relation_A_final = &struct_A_final;
+    // relation_A[0] = Radix_Sort(relation_A[0]);
+    // //
+    // // // 0.2>3499
+    // // relation_A = Filter(relation_A, 3499, '>');
+    // //
+    // Print_Relation_2(relation_A[0]);
+    //
+    // // // r0.0
+    // relation struct_B;
+    // relation * relation_B = &struct_B;
+    // Create_Relation(md,0,0,relation_A[3],1);
+    // //
+    // // // Radix Sort to r0.0
+    // relation struct_B_final;
+    // relation * relation_B_final = &struct_B_final;
+    // relation_A[3] = Radix_Sort(relation_A[3], relation_A[4]);
+    // // //
+    // //
+    //  Print_Relation_2(relation_A[1]);
+    //
+    // // // // 0.2 = 1.0
+    //  intervening * interv_final = interveningInit();
+    // Join_v2(interv_final, relation_A[0], relation_A[1], 3, 0);
+    // Print_Relation_2(interv_final->final_rel);
+    // // // 0.1
+    // Update_Relation_Keys(md,3,1,interv_final->final_rel,0);
+    // //
+    // relation struct_D_final;
+    // relation * relation_D_final = &struct_D_final;
+    // interv_final -> final_rel = Radix_Sort(interv_final->final_rel, relation_D_final);
+    // //
+    // Print_Relation_2(interv_final->final_rel);
+    // //
+    // // // r0.0
+    // relation struct_C;
+    // relation * relation_C = &struct_C;
+    // Create_Relation(md,1,0,relation_C);
+    // //
+    // // // Radix Sort to r0.0
+    // relation struct_C_final;
+    // relation * relation_C_final = &struct_C_final;
+    // relation_C = Radix_Sort(relation_C, relation_C_final);
+    // //
+    // Join_v2(interv_final, interv_final->final_rel, relation_C, 3, 1);
+    //
+    // Print_Relation_2(interv_final->final_rel);
+    // //
+    // CheckSum(md,0,2,interv_final->final_rel,1);
+    // CheckSum(md,3,1,interv_final->final_rel,0);
+    // printf("\n");
+    // for (size_t i = 0; i < interv_final->position; i++)
+    // {
+    //         printf("r%lu|",interv_final->rowId[i]);
+    // }
+//     printf("\n");
     work_line * wl_ptr;
     wl_ptr = Read_Work("workloads/small/small.work");
-    Print_Work(wl_ptr);
-    
+//    Print_Work(wl_ptr);
+
+
+    Execute_Queries(md, wl_ptr);
+
+
 
 
     // Update_Tuple_Payload(md,relation_A,1560,99999999,9999);
@@ -190,6 +268,9 @@ int main(int argc, char const *argv[])
 
     // printf("%lu\n", relation_A_final -> num_tuples);
 
+
+
+//----------------------------------------------------------------------------------------------------------------------
 
 
 
