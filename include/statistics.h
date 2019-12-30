c29355508ab4da19724e21ce38da60b3b3246381
ApChris <achris.info@gmail.com>
2019-12-30 21:58:10 +0200

    statistics.h has been changed

--- include/statistics.h
+++ include/statistics.h
@@ -1,20 +1,19 @@
-#ifndef _STATISTICS_H
-#define _STATISTICS_H
+#ifndef _STATISTICS_H_
+#define _STATISTICS_H_
 #include <stdio.h>
 #include <stdbool.h>
 #include <inttypes.h>
 #include "metadata.h"
-#include "executeQuery.h"
+
 #define N 50000000
 
 typedef struct statistics
 {
-	uint64_t * Ia;	// min value of row A
+	uint64_t * la;	// min value of row A
 	uint64_t * ua;	// max value of row A
-	uint64_t * fa;	// # values in row A
-	bool ** da; 	// # distinct values in row A
+	double * fa;	// # values in row A
+	double * da; 	// # distinct values in row A
 	uint64_t * size_da;
-	uint64_t * num_da;
 }statistics;
 
 statistics * Calculate_Statistics(metadata * md, uint64_t rows);
