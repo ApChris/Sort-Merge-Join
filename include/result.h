9a03d2ab1f2f64119c60389249e7919a729ca2a0
ApChris <achris.info@gmail.com>
2020-01-17 22:41:04 +0200

    bucket struct has been added

--- include/result.h
+++ include/result.h
@@ -2,6 +2,7 @@
 #define _RESULT_H
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "relation.h"
@@ -18,6 +19,18 @@ typedef struct result
 	uint64_t num_results;
 }result;
 
+
+typedef struct resultBucket
+{
+	uint64_t id;
+	uint64_t * payload;
+	uint64_t num_payloads;
+	bool is_null;
+}resultBucket;
+
+
+
+
 result * resultInit();
 
 void insertNewResult(result *res);
@@ -25,5 +38,10 @@ uint64_t Join(relation *rel_A, relation *rel_B, result *head);
 result * pushJoinedElements(result *head, uint64_t key, uint64_t * payload_A, uint64_t * payload_B);
 void printResult(result *res);
 void Deallocate_List(result *res);
+
+
+resultBucket * resultBucket_Init(uint64_t totalQueries);
+void resultBucket_Push(resultBucket * result,uint64_t * array_payloads,bool is_null, uint64_t pos,uint64_t num_payloads);
+
 
 #endif
